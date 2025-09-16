c734b344c33a9ef372701e79780b72ea84fd8f77
Martyanov Grigory <martyanov@ddplanet.ru>
2025-09-16 10:16:29 +0400

    Мелкие правки

--- src/Application/Cart/CartService.php
+++ src/Application/Cart/CartService.php
@@ -3,13 +3,13 @@ declare(strict_types=1);
 
 namespace Raketa\BackendTestTask\Application\Cart;
 
-use Raketa\BackendTestTask\Application\Interface\Common\UuidGeneratorInterface;
 use Raketa\BackendTestTask\Application\Interface\Repository\Cart\CartRepositoryInterface;
 use Raketa\BackendTestTask\Application\Interface\Repository\Product\ProductRepositoryInterface;
 use Raketa\BackendTestTask\Domain\Entity\Cart\Cart;
 use Raketa\BackendTestTask\Domain\Entity\Cart\CartItem;
 use Raketa\BackendTestTask\Domain\Entity\Dto\AddToCartDto;
-use Raketa\BackendTestTask\Infrastructure\Exceptions\RedisConnectorException;
+use Raketa\BackendTestTask\Domain\Entity\Product\Product;
+
 
 /**
  *
@@ -19,12 +19,10 @@ class CartService
     /**
      * @param ProductRepositoryInterface $productRepository
      * @param CartRepositoryInterface $cartRepository
-     * @param UuidGeneratorInterface $uuidGenerator
      */
     public function __construct(
         private ProductRepositoryInterface $productRepository,
         private CartRepositoryInterface $cartRepository,
-        private UuidGeneratorInterface $uuidGenerator,
     )
     {
     }
@@ -39,7 +37,6 @@ class CartService
 
         $cart = $this->getById($dto->cartId);
         $cart->addItem(new CartItem(
-            $this->uuidGenerator->generate(),
             $product->getUuid(),
             $product->getPrice(),
             $dto->quantity,
